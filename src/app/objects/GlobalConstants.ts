9e67ace4da6721904897894ec2fb8a50593e6a5d
stephengeorge568 <70499850+stephengeorge568@users.noreply.github.com>
2022-09-29 15:22:24 -0500

    Change dev api url

--- src/app/objects/GlobalConstants.ts
+++ src/app/objects/GlobalConstants.ts
@@ -1,6 +1,7 @@
 export class GlobalConstants {
     // My workstation's local ip
-    public static devServerAddress: string = "http://192.168.0.3:8080";
+    // public static devServerAddress: string = "http://localhost:8080";
+    public static devServerAddress: string = "https://stepheng.dev:8443";
     // prod ip
     public static publicServerAddress: string = "https://stepheng.dev:8443";
     public static disableStompLogging = () => {};
