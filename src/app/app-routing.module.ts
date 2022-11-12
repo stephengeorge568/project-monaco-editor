80b4e6d8db0a62628daafeb08a73e39eb642c425
Stephen George <70499850+stephengeorge568@users.noreply.github.com>
2022-11-11 20:04:08 -0600

    Switch href to routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes, { useHash: true }),
+    RouterModule.forRoot(routes, { useHash: false }),
     CommonModule
   ]
 })
